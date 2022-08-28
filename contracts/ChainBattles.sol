f04455927f8257169904bcf79590e04e5098796c
VIVEK-SUTHAR <sutharvivek18@gmail.com>
2022-08-28 18:58:06 +0530

    Week-3 challenge updated

--- contracts/ChainBattles.sol
+++ contracts/ChainBattles.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
@@ -11,21 +11,45 @@ contract ChainBattles is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    mapping(uint256 => uint256) public tokenIdToLevels;
+    // Struct for track
+    struct Track {
+        uint256 id;
+        uint256 level;
+        uint256 speed;
+        uint256 strength;
+        uint256 life;
+    }
+    // The mapping will link the NFT-Id to the stats of the NFT character.
+    mapping(uint256 => Track) public tokenIdToTrack;
 
     constructor() ERC721("Chain Battles", "CBTLS") {}
 
-    function generateCharacter(uint256 tokenId) public returns (string memory) {
+    // Define the generateCharacter function to generate the SVG image of our dynamic NFT
+    // representing a character in a game.
+    function generateCharacter(uint256 tokenId)
+        public
+        view
+        returns (string memory)
+    {
         bytes memory svg = abi.encodePacked(
             '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350">',
             "<style>.base { fill: white; font-family: serif; font-size: 14px; }</style>",
             '<rect width="100%" height="100%" fill="black" />',
-            '<text x="50%" y="40%" class="base" dominant-baseline="middle" text-anchor="middle">',
-            "Hero",
+            "<text x='50%' y='30%' class='base' dominant-baseline='middle' text-anchor='middle'>Warrior</text>",
+            "<text x='50%' y='40%' class='base' dominant-baseline='middle' text-anchor='middle'>Id:",
+            getId(tokenId),
             "</text>",
-            '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">',
-            "Levels: ",
-            getLevels(tokenId),
+            "<text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>Level:",
+            getLevel(tokenId),
+            "</text>",
+            "<text x='50%' y='60%' class='base' dominant-baseline='middle' text-anchor='middle'>Speed:",
+            getSpeed(tokenId),
+            "</text>",
+            "<text x='50%' y='70%' class='base' dominant-baseline='middle' text-anchor='middle'>Strength:",
+            getStrength(tokenId),
+            "</text>",
+            "<text x='50%' y='80%' class='base' dominant-baseline='middle' text-anchor='middle'>Life:",
+            getLife(tokenId),
             "</text>",
             "</svg>"
         );
@@ -38,12 +62,38 @@ contract ChainBattles is ERC721URIStorage {
             );
     }
 
-    function getLevels(uint256 tokenId) public view returns (string memory) {
-        uint256 levels = tokenIdToLevels[tokenId];
-        return levels.toString();
+    // get nft id
+    function getId(uint256 tokenId) public view returns (string memory) {
+        uint256 id = tokenIdToTrack[tokenId].id;
+        return id.toString();
     }
 
-    function getTokenURI(uint256 tokenId) public returns (string memory) {
+    // Create the getLevel Function to retrieve the NFT Level
+    function getLevel(uint256 tokenId) public view returns (string memory) {
+        uint256 level = tokenIdToTrack[tokenId].level;
+        return level.toString();
+    }
+
+    // Create the getSpeed Function to retrieve the NFT Speed
+    function getSpeed(uint256 tokenId) public view returns (string memory) {
+        uint256 speed = tokenIdToTrack[tokenId].speed;
+        return speed.toString();
+    }
+
+    // Create the getStrength Function to retrieve the NFT Strenght
+    function getStrength(uint256 tokenId) public view returns (string memory) {
+        uint256 strength = tokenIdToTrack[tokenId].strength;
+        return strength.toString();
+    }
+
+    // Create the getLife Function to retrieve the NFT Life
+    function getLife(uint256 tokenId) public view returns (string memory) {
+        uint256 life = tokenIdToTrack[tokenId].life;
+        return life.toString();
+    }
+
+    // Create the getTokenURI Function to generate the tokenURI
+    function getTokenURI(uint256 tokenId) public view returns (string memory) {
         bytes memory dataURI = abi.encodePacked(
             "{",
             '"name": "Chain Battles #',
@@ -64,22 +114,51 @@ contract ChainBattles is ERC721URIStorage {
             );
     }
 
-    function mint() public {
+    function mintNFT() public {
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
         _safeMint(msg.sender, newItemId);
-        tokenIdToLevels[newItemId] = 0;
+        tokenIdToTrack[newItemId].id = 1;
+        tokenIdToTrack[newItemId].level = 2;
+        tokenIdToTrack[newItemId].speed = 9;
+        tokenIdToTrack[newItemId].strength = 3;
+        tokenIdToTrack[newItemId].life = 5;
         _setTokenURI(newItemId, getTokenURI(newItemId));
     }
 
+    // train nft
     function train(uint256 tokenId) public {
         require(_exists(tokenId));
         require(
             ownerOf(tokenId) == msg.sender,
             "You must own this NFT to train it!"
         );
-        uint256 currentLevel = tokenIdToLevels[tokenId];
-        tokenIdToLevels[tokenId] = currentLevel + 1;
+        uint256 currentId = tokenIdToTrack[tokenId].id;
+        tokenIdToTrack[tokenId].id = currentId + 1;
+        uint256 currentLevel = tokenIdToTrack[tokenId].level;
+        tokenIdToTrack[tokenId].level = currentLevel + random(currentLevel);
+        uint256 currentSpeed = tokenIdToTrack[tokenId].speed;
+        tokenIdToTrack[tokenId].speed = currentSpeed + random(currentSpeed);
+        uint256 currentStrength = tokenIdToTrack[tokenId].strength;
+        tokenIdToTrack[tokenId].strength =
+            currentStrength +
+            random(currentStrength);
+        uint256 currentLife = tokenIdToTrack[tokenId].life;
+        tokenIdToTrack[tokenId].life = currentLife + random(currentLife);
         _setTokenURI(tokenId, getTokenURI(tokenId));
+    }
+
+    // for stats training
+    function random(uint256 number) public view returns (uint256) {
+        return
+            uint256(
+                keccak256(
+                    abi.encodePacked(
+                        block.timestamp,
+                        block.difficulty,
+                        msg.sender
+                    )
+                )
+            ) % number;
     }
 }
